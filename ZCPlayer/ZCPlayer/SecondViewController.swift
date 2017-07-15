748d467dd473d3d4d3450ce6459101d1e43d76ae
tieushinshin <peihsen.doyle@gmail.com>
2017-07-15 12:12:43 +0700

    temporary commit (good status)

--- ZCPlayer/ZCPlayer/SecondViewController.swift
+++ ZCPlayer/ZCPlayer/SecondViewController.swift
@@ -1,30 +1,23 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  ZCPlayer
 //
-//  Created by Doyle Illusion on 7/4/17.
+//  Created by Doyle Illusion on 7/8/17.
 //  Copyright © 2017 Zyncas Technologies. All rights reserved.
 //
 
-import Foundation
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
     
     var isInTransition = false
     
-    let urls = ["https://yosing.vn/master/files/records/records/record_record_59522512a7acd.mp4",
-                "https://yosing.vn/master/files/records/records/record_record_59505e32c8850.mp4",
-                "https://yosing.vn/master/files/records/records/record_record_5951e3e8ef2a8.mp4",
-                "https://yosing.vn/master/files/records/records/record_record_595224ff869ce.mp4",
-                "https://yosing.vn/master/files/records/records/record_record_595225066cb67.mp4"
-    ]
+    let urls = ["https://yosing.vn/master/files/records/records/record_record_59522512a7acd.mp4"]
     
     let image = UIImage.init(named: "cover.jpg")
     
     let tableView = UITableView.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height), style: .plain)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -91,7 +84,7 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController : UITableViewDataSource {
+extension SecondViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "videoCell", for: indexPath) as! VideoTableViewCell
         cell.coverImage = self.image
@@ -109,6 +102,7 @@ extension ViewController : UITableViewDataSource {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard self.isInTransition == false else { return }
+        
         let cells = self.tableView.visibleCells
         
         for cell in cells {
@@ -118,7 +112,7 @@ extension ViewController : UITableViewDataSource {
             if self.isFullyVisible(cell: videoCell, scrollView: scrollView) {
                 let url = self.urls[indexPath.row]
                 if let controller = PlayerControllerManager.shared.dict[url] {
-                    controller.addPlayerViewToSuperview(view: videoCell.contentView)
+                    controller.addPlayerViewToSuperview(view: cell.contentView)
                 }
             } else {
                 let url = self.urls[indexPath.row]
@@ -130,7 +124,7 @@ extension ViewController : UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     fileprivate func isFullyVisible(cell: VideoTableViewCell, scrollView: UIScrollView) -> Bool {
@@ -141,9 +135,8 @@ extension ViewController : UITableViewDataSource {
     }
 }
 
-extension ViewController: UITableViewDelegate {
+extension SecondViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return VideoTableViewCell.getCellSize()
+        return 200.0
     }
 }
-
